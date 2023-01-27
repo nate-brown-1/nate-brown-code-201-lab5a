b9d2b9f00fa0ce05a8dc742928f9d02fcb8b26c7
Nate Brown <nathan.d.brown@gmail.com>
2023-01-27 14:47:59 -0800

    problem 1

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
+// test 1 complete
+
 // Write your code here
 
 function sum(x, y) { //eslint-disable-line
